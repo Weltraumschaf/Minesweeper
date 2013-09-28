43486185d4d38fe37fc61f925a9b0a1e42c0cb15
Weltraumschaf <ich@weltraumschaf.de>
2013-09-28 16:56:45 +0200

    Add status panel.

---
+++ src/main/java/de/weltraumschaf/minesweeper/gui/StatusBar.java
@@ -0,0 +1,31 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+package de.weltraumschaf.minesweeper.gui;
+
+import javax.swing.JPanel;
+
+/**
+ * Status bar of game.
+ *
+ * Shows:
+ * <ul>
+ * <li>time elapsed</li>
+ * <li>games played, won, lost</li>
+ * <li>Mines not found yet</li>
+ * </ul>
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public class StatusBar extends JPanel {
+
+}
