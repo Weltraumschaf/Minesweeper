e3cc2e5570d81c67135d0b5b66c782614c97533a
Weltraumschaf <ich@weltraumschaf.de>
2013-10-09 23:38:52 +0200

    Add environment varibale for JavaFX.

--- bin/minesweeper.bat
+++ bin/minesweeper.bat
@@ -1,4 +1,18 @@
+REM
+REM  LICENSE
+REM
+REM "THE BEER-WARE LICENSE" (Revision 43):
+REM "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+REM As long as you retain this notice you can do whatever you want with
+REM this stuff. If we meet some day, and you think this stuff is worth it,
+REM you can buy me a non alcohol-free beer in return.
+REM
+REM Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+REM
+
 @echo off
 set d=%~dp0
+
+setx MINESWEEPER_JAVAFX=false
 
 start javaw -jar %d%minesweeper.jar %*
